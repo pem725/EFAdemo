32eecdb767fbd574f065b47bf88754af5ca76fdb
pem725 <pem725@gmail.com>
2017-03-13 22:21:49 -0400

    initial commit

---
+++ server.R
@@ -0,0 +1,60 @@
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(ggfortify)
+library(reshape)
+library(ggplot2)
+library(corrplot)
+library(psych)
+
+shinyServer(function(input, output) {
+  
+  GDat <- reactive({
+    NV <- round((input$Nv/input$Nc))
+    outdat <- data.frame(id=1:input$N)
+    for(i in 1:input$Nc){
+      cormat <- matrix(rbinom(NV*NV,100,input$COR)/100,NV,NV)
+      diag(cormat) <- 1
+      U <- t(chol(cormat))
+      random.normal <- matrix(rnorm(NV*input$N,0,1),NV,input$N)
+      tmp2 <- as.data.frame(t(U %*% random.normal))
+      names(tmp2) <- paste("V",1:NV,i,sep=".")
+      outdat <- cbind(outdat,tmp2)
+    }
+    outdat[,-1]
+  })
+  
+  
+  output$corPlot <- renderPlot({
+    corrplot(cor(GDat()),type="lower") 
+  })
+  
+  output$screePlot <- renderPlot({
+    scree(GDat(),F,T)
+  })
+  
+  output$compPlot <- renderPlot({
+    pout <- principal(GDat(),nfactors=input$Nr,rotate=input$rotate)
+    lout <- cbind(1:input$Nr,melt(pout$loadings[,1:input$Nr]))
+    ggplot(data=lout) + geom_bar(aes(x=X1,y=value,fill=X1),stat="identity") + facet_wrap(~X2)
+  })
+  
+  output$commPlot <- renderPlot({
+    pout <- principal(GDat(),nfactors=input$Nr,rotate=input$rotate)
+    lout <- cbind(1:input$Nr,melt(pout$loadings[,1:input$Nr]))
+    h.rn <- row.names(data.frame(pout$communality))
+    h.sq <- data.frame(Var=h.rn,h2=pout$communality)
+    ggplot(data=h.sq) + geom_bar(aes(x=Var,y=h2,fill=Var),stat="identity") + coord_flip() + xlab("Variable") + ylab("Communality") + ylim(c(0,1))
+  })
+  
+})
+
+## for testing purposes only
+#dat <- data.frame(matrix(rnorm(1000),100,10))
+#pout <- principal(dat,nfactors=2)
+#lout <- cbind(1:2,melt(pout$loadings[,1:2]))
+#ggplot(data=lout) + geom_bar(aes(x=X1,y=value,fill=X1),stat="identity") + facet_wrap(~X2)
---
+++ ui.R
@@ -0,0 +1,68 @@
+#
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Demonstrate EFA"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("COR",
+                  "Average Correlation:",
+                  min = 0,
+                  max = 1,
+                  value = .5),
+      sliderInput("N",
+                  "Number of Observations:",
+                  min = 50,
+                  max = 1000,
+                  step = 50,
+                  value = 200),
+      sliderInput("Nv",
+                  "Number of Variables:",
+                  min = 6,
+                  max = 20,
+                  value = 9),
+      sliderInput("Nc",
+                  "Number of Distinct Factors:",
+                  min = 1,
+                  max = 5,
+                  value = 1),
+      sliderInput("Nr",
+                  "Number of Factors to Retain:",
+                  min = 2,
+                  max = 6,
+                  value = 2),
+      radioButtons("rotate",
+                   "Rotation Type:",
+                   c("None" = "none",
+                     "Varimax" = "varimax",
+                     "Quatimax" = "quatimax",
+                     "Promax (Similar to SPSS)" = "promax",
+                     "Oblimin" = "oblimin",
+                     "Simplimax" = "simplimax",
+                     "Cluster" = "cluster")),
+      img(src="MRES2wbg.png",height=72),
+      "Product of the ",
+      span("MRES lab", style="color:blue")
+    ),
+    
+    # Show plots
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Correlation Plot", plotOutput("corPlot")),
+        tabPanel("Scree Plot", plotOutput("screePlot")),
+        tabPanel("Factor Plot", plotOutput("compPlot")),
+        tabPanel("Communality Plot", plotOutput("commPlot"))
+      )
+    )
+  )
+))
